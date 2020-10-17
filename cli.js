2310abcd3e54db8ecbb9e3a4c8c4d98eafe26939
Devin Witherspoon <devin.witherspoon@chanzuckerberg.com>
2020-10-17 23:09:40 +0000

    typescript working for typescript cli

--- cli.js
+++ cli.js
@@ -1,23 +1,3 @@
 #!/usr/bin/env node
-const log = require("npmlog");
 
-// stripped down version of postcss-cli that allows running this directly, and enables the inline comment monkey patch
-require("./src/utils/allow-inline-comments");
-
-// eslint-disable-next-line no-unused-expressions
-require("yargs")
-  .options({
-    loglevel: {
-      describe: "What level of logs to report.",
-      type: "string",
-      default: "info",
-    },
-  })
-  .middleware((argv) => {
-    log.level = argv.loglevel;
-  })
-  .scriptName("scss-codemods")
-  .usage("$0 <cmd> <files...>")
-  .commandDir("./src/commands")
-  .demandCommand(1)
-  .help().argv;
+require("./build/cli.js");
